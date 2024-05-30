9f11f1ba737971d131ccaa50f1cea1ddebcb0593
Dev22Patel <156922885+Dev22Patel@users.noreply.github.com>
2024-05-30 15:15:45 +0530

    p2

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,7 +2,24 @@
 
 function App() {
   return (
-    <h1>dev patel | Vite</h1>
+    <>
+    <header class="bg-white shadow">
+        <div class="container mx-auto flex justify-between items-center py-6 px-4">
+            <h1 class="text-2xl font-bold">Dev Patel</h1>
+            <nav>
+                <ul class="flex space-x-4">
+                    <li><a href="#about" class="text-gray-700 hover:text-blue-500">About</a></li>
+                    <li><a href="#skills" class="text-gray-700 hover:text-blue-500">Skills</a></li>
+                    <li><a href="#contact" class="text-gray-700 hover:text-blue-500">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+   
+
+    
+    </>
   )
 }
 
---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const add = () =>{
+    if(count <20)
+    setCount(count + 1);
+  }
+  const sub = () =>{
+    if(count > 0)
+    setCount(count - 1);
+  }
+
+  return (
+    <>
+      <button onClick={add}>Add Value : {count}</button>
+      <button onClick={sub}>Decrease value : {count}</button>
+      <footer >{count}</footer>
+    </>
+  )
+}
+
+export default App
