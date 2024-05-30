51e6c212eec8204ff364f12c2968c5863ba8fa8e
Dev22Patel <156922885+Dev22Patel@users.noreply.github.com>
2024-05-30 21:00:41 +0530

    made a component

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,12 +14,15 @@ function App() {
     if(count > 0)
     setCount(count - 1);
   }
-
   return (
     <>
-      <button onClick={add}>Add Value : {count}</button>
-      <button onClick={sub}>Decrease value : {count}</button>
+      <button onClick={add} className='bg-green-400 p-4'>Add Value : {count}</button>
+      <button onClick={sub} className='bg-red-400 p-4'>Decrease value : {count}</button>
       <footer className='bg-black font-bold rounded-xl mt-8 text-white'>{count}</footer>
+      <div className='flex flex-wrap gap-10'>
+      <Card heading="holy moly" explore="Read More"/>
+      <Card />
+      </div>
     </>
   )
 }
